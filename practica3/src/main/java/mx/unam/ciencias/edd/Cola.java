d1d2155fa1a7c496224a7d9435dffe47c369f5fe
linustorvals25 <linustorvals25@gmail.com>
2026-08-21 11:15:10 -0600

    Correciones en la clase Cola.java.

--- practica3/src/main/java/mx/unam/ciencias/edd/Cola.java
+++ practica3/src/main/java/mx/unam/ciencias/edd/Cola.java
@@ -7,26 +7,31 @@ public class Cola<T> extends MeteSaca<T> {
 
     /**
      * Regresa una representación en cadena de la cola.
+     * 
      * @return una representación en cadena de la cola.
      */
-    @Override public String toString() {
-        StringBuilder sb = new StringBuilder("");
+    @Override
+    public String toString() {
+        String toString = "";
         Nodo temp = cabeza;
         while (temp != null) {
-            sb.append(",");
+            toString += temp.elemento + ",";
+            temp = temp.siguiente;
         }
-        return sb.toString();
+        return toString;
     }
 
     /**
      * Agrega un elemento al final de la cola.
+     * 
      * @param elemento el elemento a agregar.
      * @throws IllegalArgumentException si <code>elemento</code> es
-     *         <code>null</code>.
+     *                                  <code>null</code>.
      */
-    @Override public void mete(T elemento) {
-        if (elemento ==null)
-            throw new IllegalArgumentException("La Cola no acepta a null como elemento.");
+    @Override
+    public void mete(T elemento) {
+        if (elemento == null)
+            throw new IllegalArgumentException("El elemento es nulo.");
         Nodo nuevo = new Nodo(elemento);
         if (esVacia()) {
             cabeza = rabo = nuevo;
